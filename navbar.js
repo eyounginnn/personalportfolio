34c39dd69cbc1c191ef9474750b86d4776b4e447
ericayoung <eyoung135@gmail.com>
2023-12-21 16:07:18 -0500

    style nav bar and adjust logo size

--- navbar.js
+++ navbar.js
@@ -1,12 +1,12 @@
-const Navbar = () => {
-        <div id='menu'>
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">Portfolio</a></li>
-                <li><a href="#">Contact</a></li>
-            </ul>
-        </div>
-}
-return Navbar
+// const Navbar = () => {
+//         <div id='menu'>
+//             <ul>
+//                 <li><a href="#">Home</a></li>
+//                 <li><a href="#">About</a></li>
+//                 <li><a href="#">Services</a></li>
+//                 <li><a href="#">Portfolio</a></li>
+//                 <li><a href="#">Contact</a></li>
+//             </ul>
+//         </div>
+// }
+// return Navbar
